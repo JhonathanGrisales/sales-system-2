b53410a310c3258ae74cc1ee157bfbe10f6210b6
JhonathanGrisales <jgrisalesg@gmail.com>
2021-02-28 20:41:56 -0500

    first commit

---
+++ Sales/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+
+
+namespace Sales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Store store = new Store();
+
+
+            //sellers 
+
+            Seller seller1 = new Seller();
+            seller1.Name = "Juan";
+            seller1.Age = 35;
+            store.AddSeller(seller1);
+
+            Seller seller2 = new Seller();
+            seller2.Name = "Carlos";
+            seller2.Age = 28;
+            store.AddSeller(seller2);
+
+            Seller seller3 = new Seller();
+            seller3.Name = "Andres";
+            seller3.Age = 45;
+            store.AddSeller(seller2);
+
+            //Products
+
+            Product product1 = new Product();
+            product1.Name = "mouse";
+            product1.Price = 25000;
+            store.AddProduct(product1);
+
+            Product product2 = new Product();
+            product2.Name = "keyboard";
+            product2.Price = 34000;
+            store.AddProduct(product2);
+
+            Product product3 = new Product();
+            product3.Name = "monitor";
+            product3.Price = 29000;
+            store.AddProduct(product3);
+
+            Product product4 = new Product();
+            product4.Name = "padmouse";
+            product4.Price = 48000;
+            store.AddProduct(product4);
+
+
+
+            //sales
+
+            Sale sale1 = new Sale();
+            sale1.Product = product1;
+            sale1.Seller = seller3;
+            sale1.Comments = "without discount";
+            store.AddSale(sale1);
+
+            Sale sale2 = new Sale();
+            sale2.Product = product2;
+            sale2.Seller = seller2;
+            sale2.Comments = "without discount";
+            store.AddSale(sale2);
+
+            Sale sale3 = new Sale();
+            sale3.Product = product3;
+            sale3.Seller = seller3;
+            sale3.Comments = "without discount";
+            store.AddSale(sale3);
+
+            Sale sale4 = new Sale();
+            sale4.Product = product4;
+            sale4.Seller = seller3;
+            sale4.Comments = "without discount";
+            store.AddSale(sale4);
+
+            Sale sale5 = new Sale();
+            sale5.Product = product4;
+            sale5.Seller = seller2;
+            sale5.Comments = "without discount";
+            store.AddSale(sale5);
+
+            Sale sale6 = new Sale();
+            sale6.Product = product1;
+            sale6.Seller = seller1;
+            sale6.Comments = "without discount";
+            store.AddSale(sale6);
+
+
+
+
+            //store.KnowEconomicProduct();
+
+
+
+            System.Console.WriteLine("MENU");
+            System.Console.WriteLine("1. Sales total amount ");
+            System.Console.WriteLine("2. who has sold the cheapest product ");
+            System.Console.WriteLine("3. Average price of products. ");
+            System.Console.WriteLine("4. How many units of the product have been sold. ");
+            System.Console.WriteLine("5. What's the most expensive product. \n ");
+
+
+
+            string cases = Console.ReadLine();
+            int caseSwitch = Convert.ToInt32(cases);
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Menu 1 \n");
+                    //SUMA TOTAL DE VENTAS
+                    double total = sale1.Product.Price + sale2.Product.Price + sale3.Product.Price +
+                    sale4.Product.Price + sale5.Product.Price + sale6.Product.Price;
+                    System.Console.WriteLine("Sales total amount: " + total);
+
+                    break;
+                case 2:
+                    Console.WriteLine("Menu 2 \n");
+                    //NOMBRE DEL VENDEDOR QUE VENDIO PRODUCTO MAS BARATO 
+                    store.SellerName();
+
+                    break;
+                case 3:
+                    Console.WriteLine("Menu 3 \n");
+                    //PROMEDIO DE PRODUCTOS 
+                    store.AverageProducts();
+                    break;
+                case 4:
+                    Console.WriteLine("Menu 4 \n");
+                    //CUANTOS PRODUCTOS VENDIDOS 
+                    System.Console.WriteLine("Enter any of the following: ");
+                    System.Console.WriteLine("mouse");
+                    System.Console.WriteLine("keyboard");
+                    System.Console.WriteLine("monitor");
+                    System.Console.WriteLine("padmouse \n");
+
+
+                    System.Console.WriteLine("Enter product name");
+                    string nameProduct = Console.ReadLine().ToLowerInvariant();
+                    store.SoldUnits(nameProduct);
+
+                    break;
+
+                case 5:
+                    Console.WriteLine("Menu 5 \n");
+                    //PRODUCTO MAS COSTOSO 
+                    store.ExpensiveProduct();
+                    break;
+
+                default:
+                    Console.WriteLine("Wrong value");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Sales/Store.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales
+{
+    class Store
+    {
+
+        Seller[] Sellers = new Seller[5];
+        int sellerCount = 0;
+
+        Product[] Products = new Product[6];
+        int productCount = 0;
+
+        Sale[] SalesStore = new Sale[6];
+        int saleCount = 0;
+
+        public void AddSeller(Seller newSeller)
+        {
+            Sellers[sellerCount] = newSeller;
+            sellerCount++;
+        }
+
+        public void AddProduct(Product newProduct)
+        {
+            Products[productCount] = newProduct;
+            productCount++;
+        }
+
+        public void AddSale(Sale newSale)
+        {
+            SalesStore[saleCount] = newSale;
+            saleCount++;
+            
+        }
+
+
+        //CONOCER PRODUCTO MAS ECONOMICO
+        public double KnowEconomicProduct()
+        {
+
+            //IDENTIFICO EL PRODUCTO MAS COSTOSO
+
+            double higher = 0;
+           
+
+            for (int i = 0; i < productCount; i++)
+            {
+
+
+                if (Products[i].Price > higher)
+                {
+
+                    higher = Products[i].Price;
+
+                }
+            }
+
+
+            //System.Console.WriteLine("More expensive " + higher);
+
+
+            //IDENTIFICO EL PRODUCTO MAS BARATO
+
+            double less = higher;
+            
+
+
+            for (int i = 0; i < productCount; i++)
+            {
+               
+
+                if (Products[i].Price < less)
+                {
+
+                    less = Products[i].Price;
+
+                }
+
+
+            }
+
+          
+          //System.Console.WriteLine("More economical " + less);
+
+
+            return less;
+            
+
+
+        }
+
+        //CONOCER NOMBRE VENDEDOR 
+        public void SellerName()
+        {
+
+            //PRODUCTO MAS BARATO
+            double cheap = KnowEconomicProduct();
+
+            for (int i = 0; i < saleCount; i++)
+            {
+
+                if (SalesStore[i].Product.Price == cheap)
+                {
+
+                    string name = SalesStore[i].Seller.Name;
+                                       
+                    System.Console.WriteLine("the cheapest product was sold by: " + name);
+
+                }
+
+
+            }
+
+            
+        }
+
+        //PROMEDIO DE PRODUCTOS
+        public void AverageProducts()
+        {
+            double sum = 0;
+
+            for(int i = 0; i < productCount; i++ )
+            {
+
+                sum = sum + Products[i].Price;
+
+            }
+
+            double average = sum / productCount;
+            System.Console.WriteLine("Average price of products: " + average);
+        }
+
+
+
+        //UNIDADES DE PRODUCTO VENDIDAS 
+        public void SoldUnits(String name)
+        {
+
+            int count = 0;
+
+
+            for (int i = 0; i < saleCount; i++)
+            {
+
+
+                if (SalesStore[i].Product.Name == name)
+                {
+
+                    count++;
+
+                }
+
+            }
+
+            System.Console.WriteLine("Product units sold: " + count);
+
+
+
+
+        }
+
+        //PRODUCTO MAS COSTOS 
+        public void ExpensiveProduct()
+        {
+
+
+            double higher = 0;
+            string nameProductHigher = "";
+
+
+            for (int i = 0; i < productCount; i++)
+            {
+
+
+                if (Products[i].Price > higher)
+                {
+
+                    higher = Products[i].Price;
+                    nameProductHigher = Products[i].Name;
+
+                }
+            }
+
+            System.Console.WriteLine("The most expensive product is: " + nameProductHigher);
+
+        }
+
+
+
+
+
+
+
+    }
+}
